d815c1e6f64e0efb59fceb881d9f3091fd430cfc
Matheus <matheus.guilherme@compasso.com.br>
2022-11-02 19:35:18 -0300

    Integração API

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,98 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
-import Preferencial from "./assets/cadeirante.png";
+import { MdAccessibleForward, MdListAlt, MdAttribution } from 'react-icons/md';
+
+const mock = [
+  {
+    id: "1",
+    name: "Geral",
+    type:"SG",
+    img: <MdAttribution />,
+  },
+  {
+    id: "2",
+    name: "Preferencial",
+    type: "SP",
+    img: <MdAccessibleForward />,
+  },
+  {
+    id: "3",
+    name: 'Exame',
+    type: "SE",
+    img: <MdListAlt />,
+  },
+]
+  
 
 const App = () => {
-  const [data, setData] = useState(false);
+  const [data, setData] = useState([]);
+  const [optionSelected, setOptionSelected] = useState([]);
+  const [modalConfirmVisible, setModalConfirmVisible] = useState(false);
+  const [modalPreviewVisible, setModalPreviewVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   fetch("https://ticketprint.herokuapp.com/senha/SG")
-  //     .then((res) => res.json())
-  //     .then(
-  //       (result) => {
-  //         setData(result);
-  //       },
-  //       (error) => {
-  //         console.warn("Deu erro");
-  //       }
-  //     );
-  // }, []);
+  const generateToken = (item) =>{
+    setOptionSelected(item);
+    setModalConfirmVisible(true);
+  };
+
+  const confirmGenerateToken = () =>{
+    setIsLoading(true);
+    fetch(`https://ticketprint.herokuapp.com/senha/${optionSelected.type}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setData(result);
+          setIsLoading(false)
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    setModalConfirmVisible(false);
+    setModalPreviewVisible(true);
+  }
+
   return (
-    <div class="container">
-      <div class="header">
-        <p>Sistema de Atendimento</p>
-      </div>
-      <div class="btn1">
-        <Button type={"Geral"} color={"rgba(0, 0, 255, 0.6)"} />
-      </div>
-      <div class="btn2">
-        <Button
-          type={"Preferencial"}
-          color={"rgba(0, 0, 255, 0.7)"}
-          img={Preferencial}
-        />
-      </div>
-      <div class="btn3">
-        <Button type={"Exame"} color={"rgba(0, 0, 255, 0.8)"} />
-      </div>
-      <div class="footer">
-        <div></div>
-      </div>
+    <div className='container'>
+      <header>
+        <h1>Sistema de Atendimento</h1>
+        <h3>Escolha sua ficha:</h3>
+      </header>
+      <main>
+          {mock.map(item => <Button {...item} key={item.id} onClick={() => generateToken(item)} />)}
+      </main>
+      {modalConfirmVisible && (
+        <div className='modal' >
+          <div className='modalContent'>
+            <h1>Confirmar escolha de ficha <u>{optionSelected.name}</u> ?</h1>
+            <div className='btnsModal'>
+              <button onClick={confirmGenerateToken}>Sim</button>
+              <button onClick={() => setModalConfirmVisible(false)}>Não</button>
+            </div>
+          </div> 
+        </div>     
+      )}
+      {modalPreviewVisible && (
+        <div className='modalPreviewContent'>
+          {isLoading ? 
+            <>
+              <h1>Gerando ficha...</h1>
+              <div className="spinner-container">
+                <div className="loading-spinner">
+                </div>
+              </div> 
+            </> 
+                :
+            <> 
+              <h1>Ficha gerada com sucesso!</h1>
+              <p>{data.senha}</p>
+            </>
+          }
+          <button onClick={() => setModalPreviewVisible(false)}>ok</button>
+        </div> 
+      )}
     </div>
   );
 };
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import "./style.css";
 
-const Button = ({ type, color, img }) => {
+const Button = ({ name, img, onClick }) => {
   return (
-    <button className="btn" style={{ backgroundColor: color }}>
-      {type}
-      {img && (
-        <div>
-          <img
-            src={img}
-            alt={img}
-            style={{
-              width: 40,
-              backgroundColor: "white",
-              borderRadius: 50,
-            }}
-          />
-        </div>
-      )}
+    <button className="btn" onClick={onClick} >
+      <div>
+        <p>{img}</p>
+        <h1>{name}</h1>
+      </div>
     </button>
   );
 };
